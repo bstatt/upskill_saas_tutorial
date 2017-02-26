2dad6f32e80cd0f9917a83945c229c8a5d0e8745
Britany Statt <bstatt2@gmail.com>
2017-02-26 20:14:43 +0000

    fixed the users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def job_title_icon
     if @user.profile.job_title == "Developer"
-      "<i class='fa fa-code'></i>".hmtl_safe
+      "<i class='fa fa-code'></i>".html_safe
     elsif @user.profile.job_title == "Entrepreneur"
       "<i class='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
